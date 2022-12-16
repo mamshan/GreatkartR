b88bbf1c0e2c04500e68ec58e2a8470dc9ff7196
M A M SHAN <shano1106@gmail.com>
2022-12-16 20:22:13 +0530

    2

---
+++ category/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from category.models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}  # Gợi ý trường slug theo category_name
+    list_display = ('category_name', 'slug')
+
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
